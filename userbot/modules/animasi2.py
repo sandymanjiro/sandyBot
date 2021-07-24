644835f8a7b58002c7ecf3571d30d4d077c58bef
nishfu22 <85071999+nishfu22@users.noreply.github.com>
2021-07-24 19:19:10 +0800

    Update animasi2.py

--- userbot/modules/animasi2.py
+++ userbot/modules/animasi2.py
@@ -11,7 +11,7 @@ async def typewriter(typew):
     sleep(2)
     await typew.edit("`Kedua kamu manis`")
     sleep(1)
-    await typew.edit("`Dan yang terakhir adalah kamu bukan jodohku`")
+    await typew.edit("`yg terakhir maaf aku jelek 🥺 `")
 # Create by myself @localheart
 
 
